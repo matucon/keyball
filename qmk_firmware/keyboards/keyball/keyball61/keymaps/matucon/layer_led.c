3294e1c3c5fe778a237484653f4c7ff2456f53f3
matucon <83291009+matucon@users.noreply.github.com>
2023-10-31 02:48:14 +0900

    👍 feat: レイヤーLEDをdefineで切り替えられるように修正

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/layer_led.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/layer_led.c
@@ -2,10 +2,13 @@
 
 #include "layer_led.h"
 
+#ifdef LAYER_LED_ENABLE
 static uint8_t my_latest_val = 50;
 static bool is_layer_led = false;
+#endif
 
 void change_layer_led_color(layer_state_t state) {
+#ifdef LAYER_LED_ENABLE
     if (!is_layer_led) {
         return;
     }
@@ -20,14 +23,17 @@ void change_layer_led_color(layer_state_t state) {
         case 3: rgblight_sethsv(170, rgblight_get_sat(), my_latest_val); break;
         case 4: rgblight_sethsv(  0, rgblight_get_sat(), my_latest_val); break;
     }
+#endif
 }
 
 bool toggle_layer_led(bool pressed) {
+#ifdef LAYER_LED_ENABLE
     if (pressed) {
         is_layer_led = !is_layer_led;
         if (!is_layer_led) {
             rgblight_sethsv(0, 250, 250);
         }
     }
+#endif
     return true;
 }
