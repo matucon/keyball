de826a81524cc082bffe80e691d963720035a9e1
matucon <83291009+matucon@users.noreply.github.com>
2023-10-31 18:17:39 +0900

    ⚙  config: キーマップ変更 (レイヤー2にctrl+wを追加)

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3     , KC_F4    , KC_F5   ,                                   KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , KC_PSLS  , KC_P7    , KC_P8     , KC_P9    , KC_PMNS ,                                   _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
+    _______  , KC_PSLS  , KC_P7    , KC_P8     , KC_P9    , KC_PMNS ,                                   _______  , KC_LEFT  , KC_UP    , KC_RGHT  , C(KC_W)  , KC_F12   ,
     _______  , KC_PAST  , KC_P4    , KC_P5     , KC_P6    , KC_PPLS ,                                   _______  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , MO(3)    ,
     _______  , KC_PDOT  , KC_P1    , KC_P2     , KC_P3    , KC_PEQL , S(KC_8)  ,             S(KC_9)  , _______  , KC_BTN4  , SCRL_TO  , KC_BTN5  , _______  , _______  ,
     KC_NUM   , KC_PCMM  , KC_P0    , KC_PENT   , _______  , _______ , _______  ,             KC_ESC   , _______  , _______  , _______  , _______  , _______  , KC_TRNS
