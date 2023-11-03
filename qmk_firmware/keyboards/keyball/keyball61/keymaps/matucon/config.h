6f60a429753c7fadf9336f61355db4b6c43672a8
matucon <83291009+matucon@users.noreply.github.com>
2023-11-03 19:33:31 +0900

    ⚙  config: OLEDタイムアウトを15秒から30秒に変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBALL_CPI_DEFAULT 1100
 #define KEYBALL_SCROLL_DIV_DEFAULT 5
 
-#define OLED_TIMEOUT 15000
+#define OLED_TIMEOUT 30000
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
