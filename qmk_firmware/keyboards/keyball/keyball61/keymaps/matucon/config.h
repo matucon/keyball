52a23436a71a3af7f62fcad24e9c4eff7a8fe567
matucon <83291009+matucon@users.noreply.github.com>
2023-11-10 02:08:12 +0900

    ✨ feat: ブログに記載した内容を反映

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/config.h
@@ -21,16 +21,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
 #define TAP_CODE_DELAY 5
+
+#define OLED_TIMEOUT 30000                // OLED自動消灯時間
+#define TAPPING_TERM 180                  // キー長押し時間
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5      // レイヤー数
+#define KEYBALL_CPI_DEFAULT 1100          // マウス速度
+#define KEYBALL_SCROLL_DIV_DEFAULT 5      // スクロール速度
+#define COMBO_TERM 80                     // コンボ判定時間
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // Automatic Mouse Layer 有効化
+#define AUTO_MOUSE_DEFAULT_LAYER 2        // Automatic Mouse Layer で切り替えるレイヤー番号
+#define AUTO_MOUSE_TIME 500               // Automatic Mouse Layer で元のレイヤーに戻るまでの時間
+
+// カスタム定義
+#define LAYER_LED_ENABLE                  // レイヤーLED 有効化
+#define PRECISION_ENABLE                  // プレシジョンモード 有効化
+#define PRECISION_CPI 4                   // プレシジョンモード で変更するCPI (1/100の値を指定)
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/matucon/keymap.c
@@ -20,6 +20,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+#ifdef COMBO_ENABLE
+const uint16_t PROGMEM my_bs[] = {KC_Y, KC_H, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(my_bs, KC_BSPC),
+};
+#endif
+
+enum my_keyball_keycodes {
+    LAY_TOG = KEYBALL_SAFE_RANGE, // レイヤーLEDトグル
+    PRC_TOG,                      // Precision モードトグル
+    PRC_SW,                       // Precision モードスイッチ  
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
@@ -41,24 +55,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
-    _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , _______  ,
-    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , PRC_SW   ,
+    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , PRC_TOG  ,
     _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_TOG  , LAY_TOG  , _______  , _______  , _______  , _______  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                                  RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
     _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EE_CLR  ,            EE_CLR  , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
     QK_BOOT    , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
   ),
+
+  [4] = LAYOUT_universal(
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+  ),
 };
 // clang-format on
+
+#ifdef LAYER_LED_ENABLE
+#include "layer_led.c"
+#endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
+
+    #ifdef LAYER_LED_ENABLE
+    change_layer_led_color(get_highest_layer(state));
+    #endif
+    
+    #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+    switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
+        case 1:
+            state = remove_auto_mouse_layer(state, false);
+            set_auto_mouse_enable(false);
+            break;
+        default:
+            set_auto_mouse_enable(true);
+            break;
+    }
+    #endif
+
     return state;
 }
 
@@ -72,3 +115,28 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_layerinfo();
 }
 #endif
+
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+void pointing_device_init_user(void) {
+    set_auto_mouse_enable(true);
+}
+#endif
+
+
+#ifdef PRECISION_ENABLE
+#include "precision.c"
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        #ifdef LAYER_LED_ENABLE
+        case LAY_TOG: toggle_layer_led(record->event.pressed); return true;
+        #endif
+        #ifdef PRECISION_ENABLE
+        case PRC_SW:  precision_switch(record->event.pressed); return false;
+        case PRC_TOG: precision_toggle(record->event.pressed); return false;
+        #endif
+        default: break;
+    }
+    return true;
+}
